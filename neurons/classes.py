8a2fd4d7112eb700bca702de9dec4b5fec547893
Alex Shevchuk <shevrlex@gmail.com>
2024-10-15 10:55:47 -0700

    Add FAIL_TO_PASS,PASS_TO_PASS, and test_patch to SWEBenchEntry

--- neurons/classes.py
+++ neurons/classes.py
@@ -2,9 +2,9 @@
 This is currently duplicated across taoception and gh-issue-pull repos
 TODO: remove duplication
 """
-
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
+
 
 class SWEBenchEntry(BaseModel):
     instance_id: str
@@ -15,3 +15,7 @@ class SWEBenchEntry(BaseModel):
     hints_text: Optional[str]
     created_at: str
     patch: str
+    test_patch: str
+    FAIL_TO_PASS: List[str]
+    PASS_TO_PASS: List[str]
+
