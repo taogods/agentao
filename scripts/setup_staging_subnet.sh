e4eafabd9e6d09d7ad5f0b0553fa945d12b39af9
taogods <taogod@proton.me>
2024-11-30 22:38:46 -0500

    Improve setup_staging_subnet.sh script

--- scripts/setup_staging_subnet.sh
+++ scripts/setup_staging_subnet.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-btcli config set --subtensor.network ws://127.0.0.1:9946 --wallet.path ~/.bittensor/wallets
+echo "y" | btcli config set --subtensor.network ws://127.0.0.1:9946 --wallet.path ~/.bittensor/wallets
 
 # Create subnet and owner
 btcli wallet faucet --wallet.name owner --max-successes 5 --no_prompt
-btcli subnet create --wallet.name owner --wallet.hotkey default  --no_prompt
+btcli subnet create --wallet.name owner --no_prompt
 
 # Initialize miner
 miner_address="$(jq -r ".ss58Address" ~/.bittensor/wallets/miner/coldkeypub.txt)"
 btcli wallet transfer --wallet.name owner --dest $miner_address --amount 1000 --no_prompt
-btcli subnet register --wallet.name miner --netuid 1 --wallet.hotkey default --no_prompt
+btcli subnet register --wallet.name miner --wallet.hotkey default --netuid 1 --no_prompt
 
 # Initialize validator
 validator_address="$(jq -r ".ss58Address" ~/.bittensor/wallets/validator/coldkeypub.txt)"
