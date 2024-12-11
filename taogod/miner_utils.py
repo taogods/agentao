35eaf01ac91dab526bc907a6f200969578d4172c
taogods <taogod@proton.me>
2024-12-11 13:53:18 -0500

    Update miner logs

--- taogod/miner_utils.py
+++ taogod/miner_utils.py
@@ -14,7 +14,6 @@ logger = logging.getLogger(__name__)
 class UnsolvedIssue:
     desc: str
     local_code_path: Path
-    env_setup_path: Path
 
 @dataclass
 class IssueSolution:
@@ -22,16 +21,17 @@ class IssueSolution:
 
 def create_script_arguments(
         model_name: str,
-        instance_cost: float, 
         unsolved_issue: UnsolvedIssue
     ) -> ScriptArguments:
+    swe_agent_root = Path("SWE-agent")
     return ScriptArguments(
         environment=EnvironmentArguments(
             image_name="sweagent/swe-agent:latest",
             data_path=f"text://{unsolved_issue.desc}",
             repo_path=str(unsolved_issue.local_code_path),
-            environment_setup=str(unsolved_issue.env_setup_path),
+            environment_setup=swe_agent_root / "config/environment_setup/seaborn.yaml",
             verbose=True,
+            install_environment=True,
         ),
         skip_existing=False,
         agent=AgentArguments(
@@ -39,7 +39,7 @@ def create_script_arguments(
                 model_name= model_name,
                 per_instance_cost_limit=instance_cost,
             ),
-            config_file=Path("SWE-agent/config/default_from_url.yaml"),
+            config_file=Path(swe_agent_root / "config/default_from_url.yaml"),
         ),
         actions=ActionsArguments(
             open_pr=False,
@@ -50,11 +50,10 @@ def create_script_arguments(
     )
 
 def generate_code_patch(
-        model_name: str, 
+        model_name: str,
         instance_cost: float,
-        unsolved_issue: UnsolvedIssue
     ) -> IssueSolution:
-    script_arguments = create_script_arguments(model_name, instance_cost, unsolved_issue)
+    script_arguments = create_script_arguments(model_name, instance_cost)
 
     env = SWEEnv(script_arguments.environment)
     observation, info = env.reset(0)
@@ -63,7 +62,7 @@ def generate_code_patch(
     trajectories_dir = Path.cwd() / "trajectories"
     trajectories_dir.mkdir(exist_ok=True)
 
-    logger.debug("Running sweagent...")
+    logger.info("Running sweagent...")
     info, trajectory = agent.run(
         setup_args={"issue": getattr(env, "query", None), "files": [], "test_files": [], "tests": []},
         env=env,
@@ -71,5 +70,5 @@ def generate_code_patch(
         traj_dir=trajectories_dir,
         return_type="info_trajectory",
     )
-    logger.debug(f"Finished running sweagent. Received info: {info}")
+    logger.info(f"Finished running sweagent. Received info: {info}")
     return IssueSolution(patch=info["submission"])
