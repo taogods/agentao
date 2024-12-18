b096937f1d16a6418028bf8c1c50c7135693097d
Shayan Salesi <shayansalesi@hotmail.com>
2024-12-18 10:13:04 -0500

    clean up

--- neurons/validator.py
+++ neurons/validator.py
@@ -24,7 +24,6 @@ import numpy as np
 from aiohttp import BasicAuth, ClientSession
 
 from neurons.constants import UPLOAD_ISSUE_ENDPOINT, LLM_EVAL_MULT, PROCESS_TIME_MULT
-from neurons.helpers import LOGGER
 from agentao.base.validator import BaseValidatorNeuron, TaskType
 from agentao.helpers.classes import GeneratedProblemStatement, IngestionHeuristics, \
     IssueSolution
@@ -37,21 +36,7 @@ from agentao.utils.uids import check_uid_availability
 from agentao.validator.generate_problem import create_problem_statements
 from agentao.validator.graders.abstract_grader import MinerSubmission
 from agentao.validator.graders.trueskill_grader import TrueSkillGrader
-from agentao.validator.supported_models import SUPPORTED_VALIDATOR_MODELS
-
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
-# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
-# the Software.
-# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
-# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
-
+from neurons.constants import UPLOAD_ISSUE_ENDPOINT
 
 class ValidatorDefaults:
     CODINGTASK_TIMEOUT_MINS = 30.
